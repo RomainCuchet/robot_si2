efdf46b07f1b50771742afafd6bfffa74bf85b76
RomainCuchet <47221720+RomainCuchet@users.noreply.github.com>
2021-06-03 14:18:01 +0200

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,86 @@
 def on_button_pressed_a():
     global is_active
-    is_active = 1
+    led_manager()
+    if is_active == 0:
+        is_active = 1
+    else:
+        is_active = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def line_follower():
-    if True:
-        pass
+    if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P0,
+    Kitronik_Clip_Detector.LightSelection.LIGHT) and Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P2,
+    Kitronik_Clip_Detector.LightSelection.LIGHT):
+        kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+            kitronik_klip_motor.MotorDirection.FORWARD,
+            speed_left)
+        kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
+            kitronik_klip_motor.MotorDirection.FORWARD,
+            speed_right)
+    elif Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P0,
+    Kitronik_Clip_Detector.LightSelection.DARK) and Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P2,
+    Kitronik_Clip_Detector.LightSelection.LIGHT):
+        kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+            kitronik_klip_motor.MotorDirection.REVERSE,
+            speed_left)
+        kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
+            kitronik_klip_motor.MotorDirection.FORWARD,
+            speed_right)
     elif False:
+        kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+            kitronik_klip_motor.MotorDirection.FORWARD,
+            speed_left)
+        kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
+            kitronik_klip_motor.MotorDirection.FORWARD,
+            speed_right)
+
+def on_received_string(receivedString):
+    if receivedString == "up":
         pass
-    elif False:
+    elif receivedString == "down":
         pass
+    elif receivedString == "right":
+        pass
+    elif receivedString == "left":
+        pass
+radio.on_received_string(on_received_string)
 
 def on_button_pressed_b():
-    global is_active
-    is_active = 0
+    global is_line_active
+    led_manager()
+    if is_line_active == 0:
+        is_line_active = 1
+    else:
+        is_line_active = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def led_manager():
+    if is_active == 0:
+        for x in range(5):
+            led.plot(x, 0)
+    else:
+        for x2 in range(5):
+            led.unplot(x2, 0)
+    if is_line_active == 1:
+        led.plot(1, 3)
+        led.plot(2, 4)
+        led.plot(3, 3)
+        led.plot(4, 2)
+        led.unplot(1, 2)
+        led.unplot(3, 2)
+        led.unplot(2, 3)
+        led.unplot(1, 4)
+        led.unplot(3, 4)
+    else:
+        led.unplot(1, 3)
+        led.unplot(2, 4)
+        led.unplot(3, 3)
+        led.unplot(4, 2)
+        led.plot(1, 2)
+        led.plot(3, 2)
+        led.plot(2, 3)
+        led.plot(1, 4)
+        led.plot(3, 4)
 def obstacle_detection():
     global is_obstacle
     if Kitronik_Clip_Detector.sensor_digital_detection(Kitronik_Clip_Detector.PinSelection.P1,
@@ -26,25 +91,27 @@ def obstacle_detection():
         is_obstacle = 1
 is_obstacle = 0
 is_active = 0
+speed_right = 0
+speed_left = 0
+is_line_active = 0
+led.set_brightness(100)
 serial.redirect_to_usb()
+radio.set_group(1)
 Kitronik_Clip_Detector.set_sensor_to_detect_objects()
+is_line_active = 0
 speed_left = 80
 speed_right = 80
 is_active = 0
 is_obstacle = 0
-basic.show_leds("""
-    . # # # .
-    . # . # .
-    . # # # .
-    . # # . .
-    . # . # .
-    """)
 
 def on_forever():
     obstacle_detection()
     serial.write_value("is active", is_active)
     serial.write_value("is_obstacle", is_obstacle)
-    if is_active == 1 and is_obstacle == 0:
+    serial.write_value("is_line_active", is_line_active)
+    if is_line_active == 1:
+        line_follower()
+    elif is_active == 1 and is_obstacle == 0:
         kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
             kitronik_klip_motor.MotorDirection.FORWARD,
             speed_left)
@@ -52,6 +119,7 @@ def on_forever():
             kitronik_klip_motor.MotorDirection.FORWARD,
             speed_right)
     else:
+        led_manager()
         kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR1)
         kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR2)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    led_manager()
     if (is_active == 0) {
         is_active = 1
     } else {
@@ -17,13 +18,61 @@ function line_follower () {
         kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, speed_right)
     }
 }
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "up") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, speed_left)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, speed_right)
+    } else if (receivedString == "down") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, speed_left)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, speed_right)
+    } else if (receivedString == "right") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, speed_left)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, speed_right)
+    } else if (receivedString == "left") {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, speed_left)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, speed_right)
+    }
+})
 input.onButtonPressed(Button.B, function () {
+    led_manager()
     if (is_line_active == 0) {
         is_line_active = 1
     } else {
         is_line_active = 0
     }
 })
+function led_manager () {
+    if (is_active == 0) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, 0)
+        }
+    } else {
+        for (let x2 = 0; x2 <= 4; x2++) {
+            led.unplot(x2, 0)
+        }
+    }
+    if (is_line_active == 1) {
+        led.plot(1, 3)
+        led.plot(2, 4)
+        led.plot(3, 3)
+        led.plot(4, 2)
+        led.unplot(1, 2)
+        led.unplot(3, 2)
+        led.unplot(2, 3)
+        led.unplot(1, 4)
+        led.unplot(3, 4)
+    } else {
+        led.unplot(1, 3)
+        led.unplot(2, 4)
+        led.unplot(3, 3)
+        led.unplot(4, 2)
+        led.plot(1, 2)
+        led.plot(3, 2)
+        led.plot(2, 3)
+        led.plot(1, 4)
+        led.plot(3, 4)
+    }
+}
 function obstacle_detection () {
     if (Kitronik_Clip_Detector.sensorDigitalDetection(Kitronik_Clip_Detector.PinSelection.P1, Kitronik_Clip_Detector.LightSelection.Objct)) {
         // je ne comprends pas pourquoi la détection est inversée
@@ -37,20 +86,15 @@ let is_active = 0
 let speed_right = 0
 let speed_left = 0
 let is_line_active = 0
+led.setBrightness(100)
 serial.redirectToUSB()
+radio.setGroup(1)
 Kitronik_Clip_Detector.setSensorToDetectObjects()
 is_line_active = 0
 speed_left = 80
 speed_right = 80
 is_active = 0
 is_obstacle = 0
-basic.showLeds(`
-    . # # # .
-    . # . # .
-    . # # # .
-    . # # . .
-    . # . # .
-    `)
 basic.forever(function () {
     obstacle_detection()
     serial.writeValue("is active", is_active)
@@ -58,13 +102,12 @@ basic.forever(function () {
     serial.writeValue("is_line_active", is_line_active)
     if (is_line_active == 1) {
         line_follower()
+    } else if (is_active == 1 && is_obstacle == 0) {
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, speed_left)
+        kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, speed_right)
     } else {
-        if (is_active == 1 && is_obstacle == 0) {
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, speed_left)
-            kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, speed_right)
-        } else {
-            kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
-            kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
-        }
+        led_manager()
+        kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+        kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
     }
 })
